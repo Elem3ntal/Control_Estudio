93c9f7708a73ae0acaa39e84cb71930caa15efff
Martin Palencia <martin.palencia88@gmail.com>
2018-03-11 09:18:28 -0300

    Primer commit

---
+++ control_academico/apps/gestionCursos/admin.py
@@ -0,0 +1,42 @@
+from django.contrib import admin
+from control_academico.apps.gestionCursos.models import *
+
+# Register your models here.
+
+class AdminPersonal(admin.ModelAdmin):
+    list_display = ["dni", "surnames", "names", "sex", "personaltype"]
+    list_filter = ["personaltype"]
+    #list_display_links = ["surnames"]
+    #list_editable = ["names"]
+    search_fields = ["dni"]
+    class Meta:
+        model = Personal
+
+
+class AdminCourse(admin.ModelAdmin):
+    list_display = ["cod", "name", "uc"]
+    list_filter = ["name"]
+    search_fields = ["cod"]
+    class Meta:
+        model = Course
+
+
+class AdminEnrollment(admin.ModelAdmin):
+    list_display = ["id_course", "id_student", "date_enrollment"]
+    list_filter = ["id_course"]
+    search_fields = ["id_course"]
+    class Meta:
+        model = Course
+
+class AdminTest(admin.ModelAdmin):
+    list_display = ["id_enrollment", "ptos"]
+    list_filter = ["id_enrollment"]
+    #search_fields = ["id_course"]
+    class Meta:
+        model = Course
+
+
+admin.site.register(Personal, AdminPersonal) 
+admin.site.register(Course, AdminCourse)
+admin.site.register(Enrollment, AdminEnrollment)
+admin.site.register(Test, AdminTest) 
---
+++ control_academico/apps/gestionCursos/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.db.models import Q
+
+# Create your models here.
+
+class Personal(models.Model):
+    
+    sexes = (('F','Femenino'),('M','Masculino'))
+    personal_type = (('P','Profesor'),('A','Alumno'))
+
+    surnames = models.CharField(max_length=35)
+    names = models.CharField(max_length=35)
+    dni = models.CharField(max_length=35)
+    birthdate = models.DateField(max_length=35)
+    sex = models.CharField(max_length=1, choices=sexes, default='M')
+    personaltype = models.CharField(max_length=1, choices=personal_type, default='A')
+
+    def PersonalInformation(self):
+        strings = "{0} {1}"
+        return strings.format(self.surnames, self.names)
+
+    def __str__(self):
+        return self.PersonalInformation()
+
+    def Type(self):
+        return self.personaltype == 'P'
+
+    teachertype = property(Type) 
+
+
+def limitoptions(v):
+    # De esta forma puedo filtrar los elementos por su atributo property
+    list_pks = [obj.pk for obj in Personal.objects.all() if obj.teachertype == v] 
+    return Q(pk__in = list_pks)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+    uc = models.PositiveSmallIntegerField()
+    cod = models.CharField(max_length=35)
+    status = models.BooleanField(default=True)
+    id_teachers = models.ForeignKey(Personal, models.DO_NOTHING, db_column='Id_teachers', limit_choices_to = limitoptions(True)) 
+    
+    def __str__(self):
+        return "{0}".format(self.name)
+
+
+class Enrollment(models.Model):
+    id_student = models.ForeignKey(Personal, models.DO_NOTHING, db_column='Id_student', limit_choices_to = limitoptions(False))
+    id_course = models.ForeignKey(Course, null=False, blank=False, on_delete=models.CASCADE)
+    date_enrollment = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        strings = "{0} {1}"
+        return strings.format(self.id_student, self.id_course.name)
+
+        
+class Test(models.Model):
+    id_enrollment = models.ForeignKey(Enrollment, null=False, blank=False, on_delete=models.CASCADE)
+    ptos = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        strings = "{0}, {1}"
+        return strings.format(self.id_enrollment, self.ptos)
